b8ac976b2b425e576f6c952f1dda89de7593fe05
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-03-30 14:47:16 +0300

    fix role controller

--- Year2/Sem2/Informatics/lab04/src/main/java/web/app/lab04/api/RoleController.java
+++ Year2/Sem2/Informatics/lab04/src/main/java/web/app/lab04/api/RoleController.java
@@ -3,6 +3,8 @@ package web.app.lab04.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import web.app.lab04.dbService.MyEntityManagerFactory;
 import web.app.lab04.models.Role;
 import web.app.lab04.models.User;
@@ -11,13 +13,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+
+@RestController
+@RequestMapping("role")
 public class RoleController {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @GetMapping(path = {"/", ""}, produces = "application/json")
-    public List<Role> getAllUser() {
+    public List<Role> getAllRoles() {
         return entityManager.createQuery("SELECT r FROM Role r", Role.class).getResultList();
     }
 
